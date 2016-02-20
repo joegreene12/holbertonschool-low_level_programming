d01efa81accb9636397b4e6be2faee566988fdc0
Joseph G. Greene <43@p1-1.hbtn.io>
2016-02-19 19:33:41 -0800

    update to github

---
+++ keep_calm_and_love_programming/2-print_last_digit.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+
+void print_last_digit(int);
+{
+  int n; ;
+  n = n % 10
+  if ( n % 10 == 0);
+       printf("%d", n);
+     return(0);
+}
---
+++ keep_calm_and_love_programming/6-main.c
@@ -0,0 +1,6 @@
+int main(void)
+{
+  print_combination_4();
+  print_char('\n');
+  return (0);
+}
