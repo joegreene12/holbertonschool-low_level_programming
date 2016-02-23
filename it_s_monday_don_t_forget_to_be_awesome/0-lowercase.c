38ea6c74748425989f97f10be4fdb6d7063c011c
Joseph Greene <joegreene12@github.com>
2016-02-22 23:51:42 -0800

    push for work

---
+++ it_s_monday_don_t_forget_to_be_awesome/0-lowercase.c
@@ -0,0 +1,13 @@
+
+char c (c){
+ 
+  if (c >= 'A' && c <= 'Z') {
+    printf("%c/n");
+  } else if (c >= 'a' && c <= 'z') {
+    printf("%c/n");
+  } else {
+    printf("");
+  }
+ 
+  return(0);
+}
