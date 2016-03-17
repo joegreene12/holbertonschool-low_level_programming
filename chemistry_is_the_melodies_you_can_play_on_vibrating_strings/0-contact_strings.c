b7754739f09327692fadbbd63dedaee3d3527356
Joseph Greene <joegreene12@github.com>
2016-03-16 23:39:52 -0700

    pushing updates

---
+++ chemistry_is_the_melodies_you_can_play_on_vibrating_strings/0-contact_strings.c
@@ -0,0 +1,10 @@
+char *concat_strings(char *dest, const char *src){
+
+int i;
+int i2;
+for(i=0; s1[i] != '\0'; i++){}
+for(i2=0; s2[i2] != '\0'; i2++, i++){
+  s1[i] = s2[i2];}
+  s1[i] = '\0';
+  return sl;
+}
