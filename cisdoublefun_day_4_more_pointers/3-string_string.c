563b06f9f3e9bcf71bbaa312e2e2e6fa33515712
Joseph Greene <joegreene12@github.com>
2016-05-09 14:24:52 -0700

    uploading old files

---
+++ cisdoublefun_day_4_more_pointers/3-string_string.c
@@ -0,0 +1,7 @@
+char *string_string(const char *haystack, const char *needle)
+{
+  
+
+
+
+}
---
+++ cisdoublefun_day_4_more_pointers/4-strings_compare.c
@@ -0,0 +1,12 @@
+int strings_compare(char *s1, const char *s2)
+{
+  int i;
+  int tmp;
+  i = 0;
+  
+  while ( s1[i] != '\0' || s2[i] != '\0' )
+    {
+      i++;
+    }
+
+}
